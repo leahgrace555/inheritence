415a702ddec7898270784b463a0ce2fb01763d0d
Leah <leah.grace555@gmail.com>
2020-09-21 20:47:20 -0400

    review constructor test passing

---
+++ src/main/java/inheritence/Review.java
@@ -0,0 +1,22 @@
+package inheritence;
+
+public class Review {
+    private String body;
+    private String author;
+    private int stars;
+
+    public Review(String body, String author, int stars) {
+        this.body = body;
+        this.author = author;
+        this.stars = stars;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "body='" + body + '\'' +
+                ", author='" + author + '\'' +
+                ", stars=" + stars +
+                '}';
+    }
+}
